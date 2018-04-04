4d28ef2986cf7b61d4ac07161d4f6b982ce9979a
Zach <zacharytrent@gmail.com>
2018-04-03 21:25:20 -0400

    Slightly different way of getting idt

--- arch/x86/kernel/idt.c
+++ arch/x86/kernel/idt.c
@@ -266,13 +266,9 @@ void write_custom_de_to_idt(void) {
   const static struct idt_data custom_divide_error_data = 
     INTG(X86_TRAP_DE, custom_divide_error);
   static gate_desc desc;
-  struct desc_ptr idtr;
-
-  /* Get IDT */
-  asm("sidt %0" : "=m" (idtr));
 
   idt_init_desc(&desc, &custom_divide_error_data);
-  write_idt_entry((gate_desc *)idtr.address, X86_TRAP_DE, &desc);
+  write_idt_entry(idt_table, X86_TRAP_DE, &desc);
 }
 EXPORT_SYMBOL(write_custom_de_to_idt); /* Export for common kernel code */
 
